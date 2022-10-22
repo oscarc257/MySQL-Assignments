3a956fc539e1a85a27c1cba510e79b24385f43c3
Oscar U. <112354145+oscarc257@users.noreply.github.com>
2022-10-22 11:31:58 -0700

    Week 7 Coding Assignment

---
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,14 @@
+package projects;
+
+import java.sql.Connection;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		Connection conn = DbConnection.getConnection();
+
+	}
+
+}
